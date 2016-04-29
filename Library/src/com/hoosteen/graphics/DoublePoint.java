0eb312d4b96482be9a5221b1012ecfa87f2de4e2
jtvd78 <jtvd78@aol.com>
2016-04-29 19:59:32 -0400

    First Commit. Uses a lot of code from other projects in attempt to create a Library that I can use across all of my software

---
+++ Library/src/com/hoosteen/graphics/DoublePoint.java
@@ -0,0 +1,23 @@
+package com.hoosteen.graphics;
+
+public class DoublePoint {
+	
+	double x, y;
+	
+	public DoublePoint(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX(){
+		return x;
+	}
+	
+	public double getY(){
+		return y;
+	}
+	
+	public String toString(){
+		return "(" + x + "," + y + ")";
+	}
+}
